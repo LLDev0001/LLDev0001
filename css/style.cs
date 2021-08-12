913af8cb4932398f7b7e993531aadc862613adf6
LLdev <84157624+LLDev0001@users.noreply.github.com>
2021-08-12 17:57:59 -0300

    Add files via upload

---
+++ css/style.cs
@@ -0,0 +1,72 @@
+* {
+    margin: 0px;
+    padding: 0px;
+}
+
+body {
+    background: white;
+    font-family: 'Anton', sans-serif;
+	scroll-behavior: smooth;
+}    
+
+#Ìnicio h1 {
+    color:  dodgerblue;
+}
+
+#Ìnicio {
+    font-size: center;
+    color: gray;
+}
+
+#Ìnicio img {
+    border-radius: 150px;
+	width: 25%;
+}
+ 
+#Sobre-mim p {
+    margin: 20px;
+    background: rgba(153, 153, 153, 1.00);
+    border-radius: 10px;
+    font-size: 20px;
+    color: black;
+}
+
+#Sobre-mim h1 {
+    font-size: 50px;
+    color:  dodgerblue;
+}
+
+#Redes-Sociais {
+    color:  dodgerblue;
+}
+
+#Redes-Sociais img{
+    width: 159px;
+}
+
+Redes-Sociais h1{
+	font-size: center;
+}
+
+#copy {
+    color: black;
+}
+
+.progress {
+    margin: 20px;
+}
+
+.page-link a {
+    margin: 50px;
+}
+
+button{
+    background-color: blue;
+    border: none;
+    padding: 15px;
+    width: 100%;
+    border-radius: 10px;
+    color: white;
+    font-size: 25px;
+    cursor: pointer;
+}
